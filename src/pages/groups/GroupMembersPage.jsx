86269552b23f8d69351dee2ed3a951ef2871b97e
Developersmz <developersmz@gmail.com>
2025-09-28 07:13:26 +0200

    Resolve issues

--- src/pages/groups/GroupMembersPage.jsx
+++ src/pages/groups/GroupMembersPage.jsx
@@ -5,7 +5,7 @@ import { toast } from 'sonner';
 import api from '@/services/api';
 import Avatar from '@/components/common/Avatar';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/Input';
+import { Input } from '@/components/ui/input';
 import LoadingSpinner from '@/components/common/LoadingSpinner';
 import InviteModal from '@/utils/InviteModal';
 import {
